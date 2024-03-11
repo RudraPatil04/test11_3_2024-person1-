53b8ba217f519bfab498aa90d73fb1e3a3df6503
rudra <01fe22bca037@kletech.ac.in>
2024-03-11 15:56:55 +0530

    display name and  age

---
+++ person1.java
@@ -0,0 +1,17 @@
+public class person1{
+    private String name;
+    private int age;
+    public person1(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getAge(){
+        return age;
+    
+    } 
+    
+}
